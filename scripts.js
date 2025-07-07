e18e0b37d7f0a5f4aa7532693b93b762df188704
brittojo7n <sjbritto2003@mail.com>
2025-07-07 13:39:48 +0530

    updated

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let a = 6;
+let b = 9;
+let c = a + b;
+let d = 'ab'
+let e = 'cd';
+let f = d + e;
+let g = f + c;
+
+console.log("The sum of a and b is: " + c);
+console.log("The value of g is: " + g);
