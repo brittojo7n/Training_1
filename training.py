b0d37e469757055bcd698b2a481a51fc4af86585
brittojo7n <sjbritto2003@mail.com>
2025-08-05 13:06:46 +0530

    commit

--- training.py
+++ training.py
@@ -1,21 +1,21 @@
 import time
 
-def encrypt(string, shift):
-    print(f"\nInput String: {string}")
+def encrypt(str1, shift):
+    print(f"\nInput str1: {str1}")
     result = ""
-    for char in string.lower():
-        if 'a' <= char <= 'z':
-            char_ascii = ord(char) + shift
-            if char_ascii > ord('z'):
-                char_ascii = char_ascii - 26
-            result += chr(char_ascii)
+    for str1 in str1.lower():
+        if 'a' <= str1 <= 'z':
+            str_ascii = ord(str1) + shift
+            if str_ascii > ord('z'):
+                str_ascii = str_ascii - 26
+            result += chr(str_ascii)
         else:
-            result += char
+            result += str1
     return result
 
-string = input("\nEnter a string: ")
+str1 = input("\nEnter a str1: ")
 start = time.perf_counter()
-string = encrypt(string, 4)
-print(f"Final String: {string}")
+str1 = encrypt(str1, 4)
+print(f"Final str1: {str1}")
 end = time.perf_counter()
 print(f"Execution time: {end - start:.6f} seconds")
